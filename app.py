8c5cbc46e7341657fa5597ecd2c5498226369682
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-06-11 17:36:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,44 @@
+from vllm import LLM, SamplingParams
 from transformers import AutoTokenizer
-from huggingface_hub import snapshot_download
-import os
-from pathlib import Path
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
+
 
 class InferlessPythonModel:
 
     def initialize(self):
-        model_id="facebook/opt-125m"  # Specify the model repository ID
+        model_id = "TheBloke/Llama-2-7B-chat-AWQ"  # Specify the model repository ID
         HF_TOKEN = "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" # Access Hugging Face token from environment variable
-        VOLUME_NFS = "/var/nfs-mount/llama-2-vol"  # Define model storage location
-
-        # Construct model directory path
-        model_dir = f"{VOLUME_NFS}/{model_id}"
-        model_dir_path = Path(model_dir)
-
-        # Create the model directory if it doesn't exist
-        if not model_dir_path.exists():
-            model_dir_path.mkdir(exist_ok=True, parents=True)
-
-            # Download the model snapshot from Hugging Face Hub (excluding specific file types)
-            snapshot_download(
-                model_id,
-                local_dir=model_dir,
-                token=HF_TOKEN,  # Provide token if necessary
-                ignore_patterns=["*.pt", "*.gguf"],
-            )
-
+        tokenizer_model = "meta-llama/Llama-2-7b-chat-hf"
         # Define sampling parameters for model generation
         # You can set max_tokens to 1024 for complete answer to your question
-        
-        self.model = AutoModelForCausalLM.from_pretrained(model_dir, torch_dtype=torch.float16).cuda()
-        
-        # the fast tokenizer currently does not work correctly
-        self.tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=False)
+        self.sampling_params = SamplingParams(
+            temperature=0.7,
+            top_p=0.1,
+            repetition_penalty=1.18,
+            top_k=40,
+            max_tokens=512,
+        )
 
+        # Initialize the LLM object with the downloaded model directory
+        self.llm = LLM(model=model_id, enforce_eager=True, quantization="AWQ")
+
+        # Load the tokenizer associated with the pre-trained model
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_model, token=HF_TOKEN)
 
     def infer(self, inputs):
-        prompt = inputs["prompt"]  # Extract the prompt from the input
+        prompts = inputs["prompt"]  # Extract the prompt from the input
 
-        # from transformers import AutoModelForCausalLM, AutoTokenizer
-        # import torch
-        
-        input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids.cuda()
-        
-        generated_ids = self.model.generate(input_ids)
-        
-        result = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        # Apply the chat template and convert to a list of strings (without tokenization)
+        input_text = self.tokenizer.apply_chat_template([{"role": "user", "content": prompts}], tokenize=False)
 
-        return {"generated_result": result}
+        # Generate text using the LLM with the specified sampling parameters
+        result = self.llm.generate(input_text, self.sampling_params)
+
+        # Extract the generated text from the result object
+        result_output = [output.outputs[0].text for output in result]
+
+        # Return a dictionary containing the generated text
+        return {"generated_result": result_output[0]}
 
     def finalize(self):
-        self.model = None
+        self.llm = None
         self.tokenizer = None
